852e96c2de7d017cc7a33024a2869daa88676818
NItram228 <martin.kluka.m@gmail.com>
2022-01-17 20:19:49 +0300

    ready FAQ

--- FAQ/js/faq.js
+++ FAQ/js/faq.js
@@ -1,14 +1,18 @@
-$(document).ready(function() {
-    $('.block-title').click(function(event){
-        $(this).toggleClass('active').next().slideToggle(300);
-    })  
-    $('.block-item').click(function(event){
-        $(this).toggleClass('active').next().slideToggle(300);
-    })
-    $('.block-social-icon').click(function(event){
-        $(this).toggleClass('active').next().slideToggle(300);
-    })
-    $('.number').click(function(event){
-        $(this).toggleClass('active').next().slideToggle(300);
-    })
-})
+$(document).ready(function () {
+    $('.block-item').click(function (event) {
+        $('.block-title, .block-social-icon, .block-text, .number').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $('.block-item-two').click(function (event) {
+        $('.block-title-two, .block-social-icon-two, .block-text-two, .number-two').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $('.block-item-three').click(function (event) {
+        $('.block-title-three, .block-social-icon-three, .block-text-three, .number-three').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $('.block-item-four').click(function (event) {
+        $('.block-title-four, .block-social-icon-four, .block-text-four, .number-four').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
