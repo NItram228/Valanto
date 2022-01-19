bd0074675e86f2588696ba50f916efa13c96dd5c
NItram228 <martin.kluka.m@gmail.com>
2022-01-19 20:35:57 +0300

    fixes

--- avia/js/avia.js
+++ avia/js/avia.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $('.block-item').click(function (event) {
-        $('.block-title, .block-social-icon, .block-text').toggleClass('active');
+        $('.block-title, .block-social-icon, .block-text, .block-item').toggleClass('active');
     });
     $('.block-item-two').click(function (event) {
-        $('.block-title-two, .block-social-icon-two, .block-text-two').toggleClass('active');
+        $('.block-title-two, .block-social-icon-two, .block-text-two, .block-item-two').toggleClass('active');
     });
     $('.block-item-three').click(function (event) {
-        $('.block-title-three, .block-social-icon-three, .block-text-three').toggleClass('active');
+        $('.block-title-three, .block-social-icon-three, .block-text-three, .block-item-three').toggleClass('active');
     });
 });
